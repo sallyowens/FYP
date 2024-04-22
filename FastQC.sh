b27a393b7d9a01b107a14b0342f38ac16b0a7952
sallyowens <156697494+sallyowens@users.noreply.github.com>
2024-04-22 08:15:08 +0100

    Update FastQC.sh

--- FastQC.sh
+++ FastQC.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Make new directories the raw fastq files and FASTQC output
+# User must manually import raw fastq files
 # -p: allows for any parent directory to be created if it does not alreay exist
 mkdir -p ~/FYP/script/raw_fastq_subset/FastQC
 cd ~/FYP/script/raw_fastq_subset
@@ -8,8 +9,8 @@ cd ~/FYP/script/raw_fastq_subset
 # Run FASTQC on files
 # wildcard (*): runs command on all files ending with .fq
 # --ourdir=: put the output in the desired directory (FastQC)
-fastqc *.fq --outdir=/media/newdrive/GCB2024/sallyowens/FYP/script/raw_fastq_subset/FastQC
+fastqc *.fq --outdir=~/FYP/script/raw_fastq_subset/FastQC
 
 # run MultiQC on FastQC reports to generate summary report for quality assessment on reads
-multiqc /media/newdrive/GCB2024/sallyowens/FYP/script/raw_fastq_subset/FastQC
+multiqc ~/FYP/script/raw_fastq_subset/FastQC
 
