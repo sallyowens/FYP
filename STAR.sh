4bbdfd894e0683ca5ae8f5df9a3854bad9538f11
sallyowens <156697494+sallyowens@users.noreply.github.com>
2024-04-22 08:28:02 +0100

    Update STAR.sh

--- STAR.sh
+++ STAR.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 # aligning reads to reference genome using STAR, reference genome being used is UCSC hg38
-# Genome indices had already been generated using the following code
-# STAR --runThreadN 32 --runMode genomeGenerate --genomeDir /media/newdrive/data/Reference_genomes/Human/UCSC/STAR --genomeFastaFiles /media/newdrive/data/Reference_genomes/Human/UCSC/hg38.fa --sjdbGTFfile /media/newdrive/data/Reference_genomes/Human/UCSC/hg38.ncbiRefSeq.gtf --sjdbOverhang 149
-# --runThreadN 32: to speed up the process, the tool will utilise 32 threads for the computation
+# specify STAR path, refernce genome file, and sjdbOverhang (maximum read length -1)
 for i in *_1.fq; do
-STAR --genomeDir /media/newdrive/data/Reference_genomes/Human/UCSC/STAR_UCSC_refseq/ --runThreadN 12 --readFilesIn $i ${i%_1.fq}_2.fq --readFilesCommand cat --outFileNamePrefix ${i%_1.fq} --outSAMtype BAM SortedByCoordinate --outSAMunmapped Within --outSAMattributes Standard
+STAR --genomeDir $star_path --runThreadN 12 --readFilesIn $i ${i%_1.fq}_2.fq --readFilesCommand cat --outFileNamePrefix ${i%_1.fq} --outSAMtype BAM SortedByCoordinate --outSAMunmapped Within --outSAMattributes Standard --sjdbGTFfile $referncegenome_path/hg38.ncbiRefSeq.gtf --sjdbOverhang 149
 done
 
 # Move bams and related files to a new directory and create a multiqc summary
